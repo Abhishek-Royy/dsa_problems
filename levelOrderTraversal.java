f5ea3bba5b3d2eea95b301baac38b8fee0ada910
Abhishek-Royy <abhishek699.roy@gmail.com>
2026-09-06 12:30:41 +0530

    feat: implement level order traversal with tree construction

---
+++ levelOrderTraversal.java
@@ -0,0 +1,88 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
+public class levelOrderTraversal {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int index=-1;
+
+    public static Node buildTree(int node[]){
+        index++;
+
+        if(node[index]==-1){
+            return null;
+        }
+
+        // create node
+        Node newNode=new Node(node[index]);
+
+        // left subtree
+        newNode.left=buildTree(node);
+        // right subtree
+        newNode.right=buildTree(node);
+
+        return newNode;
+    }
+
+
+    // here use Queue in the place of recursion
+    public static void levelOrderTraversal(Node root){
+        // base case
+        if(root==null){
+            return;
+        }
+
+        // crete a queue through collection frame work
+        Queue <Node> queue=new LinkedList<>();
+        
+        // add the root in the queue
+        queue.add(root);
+
+        // run a loop until q is empty
+        while(!queue.isEmpty()){
+            // remove the first element
+            Node current=queue.remove();
+
+            // print the current remove element
+            System.out.print(current.data+"->");
+
+            // add the left  element of current in the queue
+            if(current.left!=null){
+                queue.add(current.left);
+            }
+
+            // add the right element of current in the queue
+            if(current.right!=null){
+                queue.add(current.right);
+            }
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int node[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        Node tree=buildTree(node);
+
+        levelOrderTraversal(tree);
+
+    }
+}
